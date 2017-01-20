e9c97251242db41475c231f373ebd53532096d5f
anton296 <anton296@live.se>
2017-01-20 13:34:15 +0100

    Hello World
    My best one

---
+++ Hello world/Hello world/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyProgram myProgram;
+            myProgram = new MyProgram();
+            myProgram.Run();
+
+
+        }
+    }
+}
